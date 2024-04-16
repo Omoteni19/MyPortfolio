c933875c8b8991a78fac4188da898eb871864e0e
Jimoh Teniola <oluwaabidemi19@gmail.com>
2024-04-16 09:17:29 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,476 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
    
   return (
     <>
-       
-    </>
+    <section className="header">
+  
+      {/* <div className="headerContainer">
+          <div className="imageAndNameContainer">
+              <div className="imgContainer">
+                  <img className="circle" src="../assets/myimg.jpg" alt=""/>
+              </div>
+
+              <div className="nameContainer">
+                  <h3 className="Text">Teniola</h3>
+              </div>
+          </div>
+
+          <div className="menuContainer" id="menu" >
+              <img className="menu" src="./Images/menu.svg" alt=""/>
+          </div>
+
+          <div className="navigationLinksContainer" id="navigationLinksContainer">
+              <a href="#projectSection" className="projectLink" id="projectLink">
+                  <h3>My Projects</h3>
+              </a>
+              
+              <a href="./about.html" className="AboutMeLink">
+                  <h3>About Me</h3>
+              </a>
+
+              <a href="" className="ResumeLink" target="_blank">
+                  <h3>Resume</h3>
+              </a>
+          </div>
+
+          <div className="navigationLinksContainerTwo" id="navigationLinksContainerTwo">
+              <div className="iconContainer" id="closeIcon"/>
+                  <img src="./Images/close.svg" alt=""/>
+              </div>
+
+              <div className="navTwoContainer">
+                  <a href="#projectSection" className="projectLink" id="projectLink">
+                      <h3>My Projects</h3>
+                  </a>
+                  
+                  <a href="./about.html" className="AboutMeLink">
+                      <h3>About Me</h3>
+                  </a>
+
+                  <a href="" className="ResumeLink" target="_blank">
+                      <h3>Resume</h3>
+                  </a>
+              </div>
+          </div>
+      </div>
+
+
+      <div className="heroSectionContainer">
+          <div className="heroTextContainer">
+              <div className="nameAndProfessionContainer">
+                  <div className="HelloNameContainer">
+                      <h2>Hello! I'm Teniola, a</h2>
+                  </div>
+      
+                  <div className="ProfessionNameContainer">
+                      <h2>UI/UX Designer & Front-end Developer</h2>
+                  </div>
+              </div>
+
+              <div className="descriptionTextContainer">
+                  <p> Aspiring UI/UX Designer with a, passion for creating user-centerd experience. <br> Currently honing my Skills at Sail Innovation Lab.
+                      <br> where I have been able to acuminate my skills in HTML, CSS, JavaScript, Typescript and React <br>
+                      Sekking opportunities to apply my creativity and problem-solving abilities <br>to deliver impactful designs.
+                  </p>
+              </div>
+          </div>
+
+          <div className="buttonContainer">
+              <a href="#connect">
+                  <button className="connectButton">Connect With Me</button>
+              </a>
+          </div>
+
+          <div className="buttonContainerTwo">
+              <a href="#connect">
+                  <button className="connectButton">Connect With Me</button>
+              </a>
+          </div>
+      </div> */}
+    </section>
+
+{/* 
+<section className="designAndDevelopment">
+    <div className="designContainer">
+        <div className="designHeading">
+            <h2>Design</h2>
+        </div>
+
+        <div className="designDescriptionContainer">
+            <p className="deignDescription">I'm not your typical designer glued to an Illustrator artboard. Instead, I dive into code, fine-tuning fonts and pondering layouts with a (^_^) smile. My passion is crafting trendy yet great user experiences for seamless digital journeys that engage and delight.</p>
+        </div>
+    </div>
+
+    
+
+    <div className="lineContainer">
+        
+    </div>
+
+
+    <div className="developmentContainer">
+        <div className="developmentHeading">
+            <h2>Development</h2>
+        </div>
+
+        <div className="developmentDescriptionContainer">
+            <p className="developmentDescription">Blending design finesse with coding skills,  I craft products that merge aesthetics with technical efficiency, delivering swift and resilient solutions. I bring a unique blend of creativity and technical proficiency to ensure a seamless user experience.</p>
+        </div>
+    </div>
+</section> */}
+
+
+{/* 
+<section className="skillsSection">
+   <div className="skillsSectionMainDiv">
+        <div className="skillsTextContainer">
+            <h2>Technical Skills</h2>
+        </div>
+
+        <div className="skillIconContainer">
+            <div className="SkillsIcon">
+                <img className="iconsimg" src="../assets/html.png" alt="">
+                <img className="iconsimg" src="../assets/css.png" alt="">
+                <img className="iconsimg" src="../assets/javascript-logo.png" alt="">
+                <img className="iconsimg" src="../assets/typescript.png" alt="">
+                <img className="iconsimg" src="../assets/img-react-logo.png" alt="">
+            </div>
+            
+        </div>
+
+        <div className="skillIconContainertwo">
+            <div className="firstRowOfSkillsIcon">
+                <img src="../assets/html.png" alt="">
+                <img src="../assets/css.png" alt="">
+            </div>
+
+            <div className="firstRowOfSkillsIcon">
+                <img src="../assets/javascript-logo.png" alt="">
+                <img src="../assets/typescript.png" alt="">
+            </div>
+
+            <div className="firstRowOfSkillsIcon">
+                <img src="../assets/img-react-logo.png" alt="">
+            </div>
+
+        </div>
+   </div>
+</section> */}
+
+{/* 
+<section className="projectSection" id="projectSection">
+    <div className="projectTextAndDescriptionContainer">
+        <div className="projectTextContainer">
+            <h2>Project</h2>
+        </div>
+
+        <div className="projectdescriptionContainer">
+            <p>My Curated Architectures</p>
+        </div>
+    </div>
+
+    <div className="projectsContainer">
+        <div className="firstRowOfProjects">
+            <div className="projectContainer">
+                <div className="imageContainerRandomQuotes">
+
+                </div>
+
+                <div className="titleDescriptionAndLinkContainer">
+                    <div className="titleAndDescriptionContainer">
+                        <div className="titleContainer">
+                            <h3>Random Quotes Generator</h3>
+                        </div>
+
+                        <div className="descriptionContainer">
+                            <p>Click the button, get a quote! it's that easy! Discover simple and uplifting messages with just one click.</p>
+                        </div>
+                    </div>
+
+                    <a href="https://bit.ly/randomquotegenerator" className="viewProject" target="_blank">
+                        <p>View Project</p>
+                    </a>
+                </div>
+            </div>
+
+            <div className="projectContainer">
+                <div className="imageContainerUnsplash">
+
+                </div>
+
+                <div className="titleDescriptionAndLinkContainer">
+                    <div className="titleAndDescriptionContainer">
+                        <div className="titleContainer">
+                            <h3>Unsplash Website</h3>
+                        </div>
+
+                        <div className="descriptionContainer">
+                            <p>A polished and professional landing page mirroring the design of the Unsplash website with precision and accuracy.
+                            </p>
+                        </div>
+                    </div>
+
+                    <a href="https://preshchukwu.github.io/Unsplash-clone/" className="viewProject" target="_blank">
+                        <p>View Project</p>
+                    </a>
+                </div>
+            </div>
+
+            <div className="projectContainer">
+                <div className="imageContainerNote">
+
+                </div>
+
+                <div className="titleDescriptionAndLinkContainer">
+                    <div className="titleAndDescriptionContainer">
+                        <div className="titleContainer">
+                            <h3>Note App</h3>
+                        </div>
+
+                        <div className="descriptionContainer">
+                            <p>
+                                Effortlessly capture your thoughts, ideas, and more in a user-friendly note-taking interface designed for ease of use. </p>
+                        </div>
+                    </div>
+
+
+                    <a href="https://preshchukwu.github.io/Note-App/" className="viewProject" target="_blank">
+                        <p>View Project</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="firstRowOfProjects">
+            <div className="projectContainer">
+                <div className="imageContainerColorGenerator">
+
+                </div>
+
+                <div className="titleDescriptionAndLinkContainer">
+                    <div className="titleAndDescriptionContainer">
+                        <div className="titleContainer">
+                            <h3>Random Color Generator</h3>
+                        </div>
+
+                        <div className="descriptionContainer">
+                            <p>Effortlessly generate diverse color palettes with a single click </p>
+                        </div>
+                    </div>
+
+                    <a href="https://preshchukwu.github.io/Color-Generator/" className="viewProject" target="_blank">
+                        <p>View Project</p>
+                    </a>
+                </div>
+            </div>
+
+            <div className="projectContainer">
+                <div className="imageContainerRayn">
+
+                </div>
+
+                <div className="titleDescriptionAndLinkContainer">
+                    <div className="titleAndDescriptionContainer">
+                        <div className="titleContainer">
+                            <h3>Rayn Craft Landing Page</h3>
+                        </div>
+
+                        <div className="descriptionContainer">
+                            <p>Landing page for an interior decoration agency</p>
+                        </div>
+                    </div>
+
+                    <a href="https://rayn-interiors1.vercel.app/" className="viewProject" target="_blank">
+                        <p>View Project</p>
+                    </a>
+                </div>
+            </div>
+
+            <div className="projectContainer">
+                <div className="imageContainerPrecious">
+
+                </div>
+
+                <div className="titleDescriptionAndLinkContainer">
+                    <div className="titleAndDescriptionContainer">
+                        <div className="titleContainer">
+                            <h3>Personal Website</h3>
+                        </div>
+
+                        <div className="descriptionContainer">
+                            <p>Explore my professional online portfolio, showcasing my skills and experiences in a user-friendly and polished personal website. </p>
+                        </div>
+                    </div>
+
+                    <a href="./index.html" className="viewProject">
+                        <p>View Project</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> */}
+
+{/*  */}
+{/* <section className="connectWithMeSection" id="connect">
+   <div className="connectWithMeSectionContentsContainer">
+        <div className="connectWithMeTopSection">
+            <div className="connectWithMeTextContainer">
+                <h2>Connect With Me</h2>
+            </div>
+
+            <div className="connectWithMeDescriptionTextContainer">
+                <p>Got an idea that's begging to break free? Shoot me a message, let's craft something epic together!</p>
+            </div>
+        </div>
+
+        <div className="connectWithMeBottomSection">
+            <div className="connectWithMeBottomLeftContainer">
+                <form action="https://api.web3forms.com/submit" method="POST" className="form" id="form">
+                    <div className="formContents">
+
+                        <input type="hidden" name="access_key" value="154cf22a-5a0e-44b8-aa10-1f3eea8bcf99">
+
+                        <div className="nameContainer">
+                            <label for="yourName"> Enter Your Name</label>
+                            <input className="nameInputField" type="text" id="yourName" name="Name" placeholder="Name">
+                        </div>
+    
+                        <div className="emailContainer">
+                            <label for="yourEmail"> Enter Your Email Address</label>
+                            <input className="emailInputField" type="email" id="yourEmail" name="email" placeholder="Email Address">
+                        </div>
+    
+                        <div className="textAreaContainer">
+                            <label for="yourMessage"> Enter Your Message</label>
+                            <textarea className="textareaInputField" name="message" placeholder="Your Message" id="yourMessage" cols="30" rows="10"></textarea>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="sendMessageButton" id="inputButton"> Send Message</button>
+                    
+                </form>
+            </div>
+
+            <div className="connectWithMeBottomRightContainer">
+
+            </div>
+        </div>
+   </div>
+    
+</section> */}
+
+{/* 
+<section className="footer" >
+    <div className="footerContentsContainer">
+        <div className="footerTopContentsContainer">
+            <div className="footerLeftContentsContainer">
+                <div className="myNameContainer">
+                    <h3>PRECIOUS NWACHUKWU</h3>
+                </div>
+
+                <div className="myDescriptionContainer">
+                    <p>Dedicated Frontend Web Developer specializing in crafting compelling and user-friendly interfaces for websites and web applications. Committed to enhancing overall product success through seamless and intuitive frontend experiences.</p>
+                </div>
+            </div>
+
+            <div className="footerRightContentsContainer">
+                <div className="footerSocialTextContainer">
+                    <h3>SOCIALS</h3>
+                </div>
+
+                <div className="footerLinkImagesContainer">
+                    <a href="mailto:preciousnwachukwu.c@gmail.com" className="socialLink" target="_blank">
+                        <img src="./Images/basil_gmail-solid.svg" alt="">
+                    </a>
+
+                    <a href="https://github.com/Preshchukwu" className="socialLink" target="_blank">
+                        <img className="socialIcon" src="./Images/ant-design_github-outlined.svg" alt="">
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/preciouschukwu/" className="socialLink" target="_blank">
+                        <img src="./Images/bi_linkedin.svg" alt="">
+                    </a>
+
+                    <a href="https://twitter.com/Presh_chukwu" className="socialLink" target="_blank">
+                        <img src="./Images/pajamas_twitter.svg" alt="">
+                    </a>
+
+                    <a href="https://www.behance.net/preciouschukwu" className="socialLink" target="_blank">
+                        <img src="./Images/mage_behance.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div className="footerBottomContentsContainer">
+            <div className="footerLineContainer">
+                <hr className="line">
+            </div>
+
+            <div className="copyRightContainer">
+                <p>&copy 2024. Made by Precious Nwachukwu</p>
+            </div>
+        </div>
+    </div>
+</section> */}
+{/* 
+<section className="footerTwo" >
+    <div className="footerContentsContainer">
+        <div className="footerTopContentsContainer">
+            <div className="footerRightContentsContainer">
+                <div className="footerSocialTextContainer">
+                    <h3>SOCIALS</h3>
+                </div>
+
+                <div className="footerLinkImagesContainer">
+                    <a href="mailto:preciousnwachukwu.c@gmail.com" className="socialLink" target="_blank">
+                        <img src="./Images/basil_gmail-solid.svg" alt="">
+                    </a>
+
+                    <a href="https://github.com/Omoteni19/" className="socialLink" target="_blank">
+                        <img className="socialIcon" src="./Images/ant-design_github-outlined.svg" alt="">
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/teniola-jimoh-8b0287255/" className="socialLink" target="_blank">
+                        <img src="./Images/bi_linkedin.svg" alt="">
+                    </a>
+
+                    <a href="https://twitter.com/OMOTENI19" className="socialLink" target="_blank">
+                        <img src="./Images/pajamas_twitter.svg" alt="">
+                    </a>
+
+                </div>
+            </div>
+
+            <div className="footerLeftContentsContainer">
+                <div className="myNameContainer">
+                    <h3>PRECIOUS NWACHUKWU</h3>
+                </div>
+
+                <div className="myDescriptionContainer">
+                    <p>Dedicated Frontend Web Developer specializing in crafting compelling and user-friendly interfaces for websites and web applications. Committed to enhancing overall product success through seamless and intuitive frontend experiences.</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="footerBottomContentsContainer">
+            <div className="footerLineContainer">
+                <hr className="line">
+            </div>
+
+            <div className="copyRightContainer">
+                <p>&copy 2024. Made by Precious Nwachukwu</p>
+            </div>
+        </div>
+    </div>
+</section>
+        */}
+</>
   )
 }
 
